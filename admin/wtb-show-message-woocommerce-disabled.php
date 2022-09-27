938e89877d464028983bb4fa3d6dd88235bd911a
Pedroemanuelavelar <pedroemanuel5296@gmail.com>
2022-09-27 13:40:05 -0300

    Adicionado verificação de olugin woocommerce

---
+++ admin/wtb-show-message-woocommerce-disabled.php
@@ -0,0 +1,33 @@
+<?php
+add_action( 'admin_init', 'wtb_plugin_check_if_woocommerce_installed' );
+function wtb_plugin_check_if_woocommerce_installed() {
+
+    // If WooCommerce is NOT installed, Deactivate the plugin
+    if ( is_admin() && current_user_can( 'activate_plugins') && !is_plugin_active( 'woocommerce/woocommerce.php') ) {
+
+        // Show dismissible error notice
+        add_action( 'admin_notices', 'wtb_plugin_woocommerce_check_notice' );
+
+        // Deactivate this plugin
+        deactivate_plugins( plugin_basename( __FILE__) );
+        if ( isset( $_GET['activate'] ) ) {
+            unset( $_GET['activate'] );
+        }
+    }
+    // If WooCommerce is installed, activate the plugin and carry out further processing
+    elseif ( is_admin() && is_plugin_active( 'woocommerce/woocommerce.php') ) {
+
+        // Carry out further processing here
+    }
+}
+
+// Show dismissible error notice if WooCommerce is not present
+function wtb_plugin_woocommerce_check_notice() {
+    ?>
+    <div class="alert alert-danger notice is-dismissible">
+        <p>Sorry, but this plugin requires WooCommerce in order to work.
+            So please ensure that WooCommerce is both installed and activated. Please <a href="http://localhost:10058/wp-admin/update.php?action=install-plugin&plugin=woocommerce&_wpnonce=9fa1191bdf">install and activate</a> 
+        </p>
+    </div>
+    <?php
+}
--- woo-taxonomy-brands.php
+++ woo-taxonomy-brands.php
@@ -3,7 +3,7 @@
  * Plugin Name: WooCommerce Taxonomy Brands
  * Plugin URI: https://pedroavelar.com.br
  * Description: Um plugin para adicionar taxonomia de marcas nos produtos, aparecendo diretamente na single do produto.
- * Version: 0.0.1
+ * Version: 1.0
  * Author: Pedro Avelar
  * Author URI: https://pedroavelar.com.br
  * License: GNU General Public License v3.0
@@ -17,7 +17,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 define('WTB_PLUGIN_PREFIX', 'wtb_');
-define('WTB_PLUGIN_VERSION', '0.0.1');
+define('WTB_PLUGIN_VERSION', '1.0');
 define('WTB_PLUGIN_DIR', plugin_dir_path( dirname(__FILE__) ) . 'woo-taxonomy-brands/');
 
 if ( ! defined( 'WTB_PLUGIN_BASE' ) ) {
@@ -27,6 +27,8 @@ if ( ! defined( 'WTB_PLUGIN_BASE' ) ) {
 require_once WTB_PLUGIN_DIR . '/admin/wtb-settings-plugin.php';
 require_once WTB_PLUGIN_DIR . '/admin/cpt/wtb-custom-post-type.php';
 require_once WTB_PLUGIN_DIR . '/admin/wtb-show-on-single-product.php';
+require_once WTB_PLUGIN_DIR . '/admin/wtb-show-message-woocommerce-disabled.php';
+
 
 
 
