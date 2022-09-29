b30fa436542e49944a6cdcb3723b437fc296754a
Pedroemanuelavelar <pedroemanuel5296@gmail.com>
2022-09-29 10:26:27 -0300

    Remoção de plugin uri

--- brand-taxonomy-for-your-store.php
+++ brand-taxonomy-for-your-store.php
@@ -1,7 +1,6 @@
 <?php
 /**
  * Plugin Name: Brand taxonomy for your store
- * Plugin URI: https://pedroavelar.com.br
  * Description: An additional plugin for woocommerce to add taxonomies of brands to products, appearing directly in the single of the product.
  * Version: 1.0
  * Author: Pedro Avelar
